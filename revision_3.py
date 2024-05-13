47bb55ce21b1f9ba2a8c6eac4a5c24aa77003c3a
Some Guy <122555673+Xpath1@users.noreply.github.com>
2024-05-13 13:49:01 -0400

    revision 4

--- revision_3.py
+++
@@ -1,3 +0,0 @@
-import os
-os.system("git clone https://github.com/chocolate-doom/chocpkg.git")
-os.system("./chocpkg/chocpkg build chocolate-doom/latest")
---
+++ revision_4.py
@@ -0,0 +1,5 @@
+import os
+os.system("git clone https://github.com/chocolate-doom/chocpkg.git")
+os.system("./chocpkg/chocpkg build chocolate-doom/latest")
+
+os.system("chocpkg info")
