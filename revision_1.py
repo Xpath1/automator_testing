c6118ffe7d1f87714e6f87b57b31810da2d4a3f9
Some Guy <122555673+Xpath1@users.noreply.github.com>
2024-05-13 13:35:54 -0400

    revision 2

--- revision_1.py
+++
@@ -1,6 +0,0 @@
-import os
-os.system("""git clone https://github.com/chocolate-doom/chocolate-doom.git
-cd chocolate-doom
-./autogen.sh
-make
-make install""")
---
+++ revision_2.py
@@ -0,0 +1,6 @@
+import os
+os.system("git clone https://github.com/chocolate-doom/chocolate-doom.git")
+os.system("cd chocolate-doom")
+os.system("./autogen.sh")
+os.system("make")
+os.system("make install")
